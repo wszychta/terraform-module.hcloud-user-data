735fa831356b47c29d54357119ea74f19e7645b9
Wojciech Szychta <wojtek.szychta@gmail.com>
2023-01-30 19:46:37 +0100

    Update output

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-# Common outputs
-
-output "result_file" {
-  value = local.result_user_data_file
-}
-
-output "result_hosts_file" {
-  value = local.additional_hosts_entries_file
-}
-
-# interfaces.d outputs
-
 /*
 Terraform module for creating Hetzner cloud compatible user-data file
 Copyright (C) 2021 Wojciech Szychta
@@ -27,6 +15,23 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
+# Common outputs
+
+output "result_file" {
+  value = local.result_user_data_file
+}
+
+output "result_hosts_file" {
+  value = local.additional_hosts_entries_file
+}
+
+output "packages_install_script" {
+  value = local.packages_install_script_file
+}
+
+# interfaces.d outputs
+
 output "result_interfacesd_file_map" {
   value = local.interfaced_cloud_config_file_map
 }
@@ -51,10 +56,6 @@ output "netplan_network_file" {
 
 output "netplan_network_merge_script" {
   value = local.netplan2_merge_script_file
-}
-
-output "netplan_packages_install_script" {
-  value = local.netplan2_packages_install_script_file
 }
 
 # ifcfg outputs
